f64c3e4d46ac18b5dc87972c1dae02e448f390a6
Deepak Selvan <dselvan@gmail.com>
2018-02-19 19:51:09 -0800

    remove inception bin and add script for running the retrain

---
+++ tf_files/retrain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo docker run -t \
+    -v "$(pwd)":/tf_files \
+    -w /tensorflow \
+     tensorflow/tensorflow:latest-devel \
+     python tensorflow/examples/image_retraining/retrain.py \
+        --bottleneck_dir=/tf_files/bottlenecks \
+        --how_many_training_steps 500 \
+        --model_dir=/tf_files/inception \
+        --output_graph=/tf_files/retrained_graph.pb \
+        --output_labels=/tf_files/retrained_labels.txt \
+        --image_dir /tf_files/pokemon/
